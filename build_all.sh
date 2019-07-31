2f6cf1bad7cfb1b15dc7c74db079cf048c123926
John Ewart <john@yourbase.io>
2019-07-31 16:40:32 -0700

    Added release

--- build_all.sh
+++ build_all.sh
@@ -1,4 +1,9 @@
 VERSIONS=$(ruby-build --definitions | grep "^${PREFIX}")
+OS=$(uname -s)
+ARCH=$(uname -m) 
+VERSION=$(uname -r)
+
+mkdir -p ./archives 
 
 for i in ${VERSIONS}; do
   if [ ! -d ./$i ]; then
@@ -7,4 +12,7 @@ for i in ${VERSIONS}; do
   else
     echo "Already built $i"
   fi
+  TARFILE="ruby-$i-$OS-$ARCH-$VERSION.tar.bz2"
+  echo "Compressing $i as $TARFILE..."
+  tar jcf ${TARFILE} ./archives/${i}
 done
