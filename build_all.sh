1a67df25a52e2af9143b34bf8497f9fc20d0333e
John Ewart <john@johnewart.net>
2019-08-10 11:26:14 -0700

    Try older OpenSSL on bionic

--- build_all.sh
+++ build_all.sh
@@ -13,24 +13,48 @@ fi
 
 mkdir -p ./archives 
 echo "Versions: ${VERSIONS}"
-export CONFIGURE_OPTS="--disable-install-doc --enable-load-relative"
+export DEFAULT_CONFIGURE_OPTS="--disable-install-doc --enable-load-relative"
 
+FAILED=()
+SUCCESSFUL=()
+BUILD_DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null 2>&1 && pwd )"
 for i in ${VERSIONS}; do
+  RUBY_DIR="${BUILD_DIR}/$i"
   TARFILE="ruby-$i-$OS-$ARCH-$OS_VERSION.tar.bz2"
   IN_S3=$(curl -s -I https://yourbase-build-tools.s3-us-west-2.amazonaws.com/ruby/$TARFILE | grep 200)
   if [ "$?" == "0" ]; then 
     echo "$TARFILE already exists in S3!"
   else 
-    if [ ! -d ./$i ]; then
-      echo "Will build $i..."
-      ruby-build $i ./$i
+    if [ ! -d ${RUBY_DIR} ]; then
+      echo "Will build Ruby $i..."
+      mkdir -p $i
+      RUBY_CONFIGURE_OPTS="${DEFAULT_CONFIGURE_OPTS}"
+
+      if [[ "$i" =~ "2.1" ]] || [[ "$i" =~ "2.0" ]] || [[ "$i" =~ "2.2" ]] || [[ "$i" =~ "2.3" ]]; then 
+        echo "Installing OpenSSL 1.0 into ${RUBY_DIR}"
+        wget https://www.openssl.org/source/openssl-1.0.2s.tar.gz
+        tar zxvf openssl-1.0.2s.tar.gz
+        (cd openssl-1.0.2s && ./config -fPIC --openssldir=${RUBY_DIR} --prefix=${RUBY_DIR} && make && make install) 
+        if [ "$?" != "0" ]; then 
+          echo "Failed to install OpenSSL 1.0, will try next Ruby version"
+          continue
+        fi
+
+        RUBY_CONFIGURE_OPTS="${RUBY_CONFIGURE_OPTS} --with-openssl-dir=${RUBY_DIR} --with-gcc=gcc"
+      fi 
+
+      ruby-build $i ${RUBY_DIR}
+      if [ "$?" == "0"]; then 
+        SUCCESSFUL+=( "$i" )
+        OUTFILE="./archives/${TARFILE}"
+        echo "Done! Compressing $i as $OUTFILE..."
+        tar jcf ${OUTFILE} ${i}
+      else   
+        FAILED+=( "$i" )
+      fi
     else
       echo "Already built $i"
     fi
-
-    OUTFILE="./archives/${TARFILE}"
-    echo "Compressing $i as $TARFILE..."
-    tar jcf ${OUTFILE} ${i}
   fi 
 done
 
